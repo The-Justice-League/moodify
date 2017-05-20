57274f21ba54747116fcf43eab87f6c108a21190
James Hogan <jhogan@jhogan.net>
2017-05-19 19:38:24 -0700

    Add client-side moodification functionality

--- client/src/components/Recommendations.jsx
+++ client/src/components/Recommendations.jsx
@@ -1,35 +1,27 @@
 import React from 'react';
+import Recommendation from './Recommendation.jsx';
 
 class Recommendations extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  changeSong(uri, songName, artistName) {
-    this.props.processRecommendation(uri, songName, artistName);
-    console.log('clicked - value of uri === ', uri)
-  }
-
   render() {
-    console.log('Props in Recommendations === ', this.props)
-    // console.log('Show lyrics is currently set to === ', this.props.showLyrics)
     return (
       <div >
         <h2>Recommendations</h2>
-        <h6 onClick={this.changeSong.bind(this, this.props.dummyd[0].uridummy, this.props.dummyd[0].songName, this.props.dummyd[0].artistName)} >
-        {this.props.dummyd[0].songName + ' ' + this.props.dummyd[0].artistName}</h6>
-
-        <h6 onClick={this.changeSong.bind(this, this.props.dummyd[1].uridummy, this.props.dummyd[1].songName, this.props.dummyd[1].artistName)} >
-        {this.props.dummyd[1].songName + ' ' + this.props.dummyd[1].artistName}</h6>
+        { this.props.recommendations.map( (recommendation, idx) =>
+          <Recommendation
+            processRecommendation={this.props.processRecommendation}
+            uri={recommendation.uri}
+            artistName={recommendation.artistName}
+            songName={recommendation.songName}
+            key={idx}
+          />
+        )}
       </div>
     );
   }
 };
 
 export default Recommendations;
-
-/*
-<h6>{this.props.songNameAndArtist[0] + ' - ' + this.props.songNameAndArtist[1]}</h6>
-
-
-*/
